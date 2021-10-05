52284e25e47419f889c7103985c9bbf78e06ef20
mrjackmaster <jacektrg+github@gmail.com>
2021-10-05 23:04:30 +0200

    IDs of some example videos to use for planning layout.

---
+++ src/components/data/trending.js
@@ -0,0 +1,28 @@
+const trending = [
+	{
+		id: 'cExLQ1o2pDw',
+	},
+	{
+		id: 'GeTEXzR-vPE',
+	},
+	{
+		id: 'KT18KJouHWg',
+	},
+	{
+		id: 'LmdAcPOIZ-8',
+	},
+	{
+		id: 'xP3QYSQk7fk',
+	},
+	{
+		id: 'NPPGWtHEQO0',
+	},
+	{
+		id: 'oYU5HfG-TPo',
+	},
+	{
+		id: 'FwoJkxtmCjw',
+	},
+];
+
+export default trending;
---
+++ src/components/data/videos.js
@@ -0,0 +1,40 @@
+const videos = [
+	{
+		id: 'Dx5qFachd3A',
+	},
+	{
+		id: '6gdjHOT4xpg',
+	},
+	{
+		id: 'b-h4b3NTcJ0',
+	},
+	{
+		id: 'LlBVIpEQGIs',
+	},
+	{
+		id: 'oqxAJKy0ii4',
+	},
+	{
+		id: '-DveLw6yyHw',
+	},
+	{
+		id: 'YtcaS5dxdLo',
+	},
+	{
+		id: '64CsCJtOKt4',
+	},
+	{
+		id: 'd3sdpcaMbPk',
+	},
+	{
+		id: 'G5VQUDpK9Iw',
+	},
+	{
+		id: 'cEItmb_a20M',
+	},
+	{
+		id: '9qTzrQ1DzmQ',
+	},
+];
+
+export default videos;
