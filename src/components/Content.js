0ed72f999de357d43aca24b56f1f44668cd62e90
mrjackmaster <jacektrg+github@gmail.com>
2021-10-05 23:10:01 +0200

    Component displaying videos feed on main page with some placeholder filters.

---
+++ src/components/Content.js
@@ -0,0 +1,36 @@
+const Content = ({ content, trending }) => {
+	return (
+		<div id='content-container'>
+			<div id='category-filter'>
+				<ul>
+					<li>All</li>
+					<li>Playlists</li>
+					<li>Music</li>
+					<li>Jazz</li>
+					<li>Live</li>
+					<li>Computers</li>
+					<li>Javascript</li>
+					<li>Piano</li>
+					<li>HTML5</li>
+					<li>Unboxing</li>
+					<li>Apple</li>
+					<li>CSS</li>
+					<li>Editing</li>
+					<li>Sales</li>
+					<li>Recently uploaded</li>
+					<li>Watched</li>
+					<li>New to you</li>
+				</ul>
+			</div>
+			<div className='content-display'>
+				{content}
+				<div className='trending-content-display'>
+					<h2>Trending</h2>
+					{trending}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Content;
