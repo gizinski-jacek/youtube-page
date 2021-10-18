7bedecb3be95c2c59bd5ab72dac35ff0b6e2274a
mrjackmaster <jacektrg+github@gmail.com>
2021-10-18 19:15:57 +0200

    Moving content card wrapper to its own module.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import getRandomVideosFromFirestore from './components/utils/getRandomVideosFrom
 import getYTTrendingVideos from './components/utils/getYTTrendingVideos';
 import getYTVideoStatistics from './components/utils/getYTVideoStatistics';
 import getYTChannelData from './components/utils/getYTChannelData';
+import ContentCardWrapper from './components/utils/ContentCardWrapper';
 
 const App = () => {
 	const [menuIsThin, setMenuIsThin] = useState(false);
@@ -36,12 +37,12 @@ const App = () => {
 		setSearchInput(value);
 	};
 
-	const loadVideo = (vidData) => {
-		setLoadedVideoData(vidData);
-	};
-
 	const collapseMenu = () => {
 		setMenuIsCollapsed((prevState) => !prevState);
+	};
+
+	const loadVideo = (vidData) => {
+		setLoadedVideoData(vidData);
 	};
 
 	useEffect(() => {
@@ -70,60 +71,13 @@ const App = () => {
 				return Promise.all([videoStats, channelData]).then(
 					([stats, channel]) => {
 						return (
-							<Link
-								to={`watch=${video.videoData.id.videoId}`}
-								key={index}
-								className='content-card'
-								onClick={() =>
-									loadVideo({ video, stats, channel })
-								}
-							>
-								<img
-									className='card-thumbnail'
-									src={
-										video.videoData.snippet.thumbnails
-											.medium.url
-									}
-									alt='Video thumbnail'
-								/>
-								<div className='card-details'>
-									<img
-										className='channel-picture'
-										src={
-											channel.thumbnails.default.url ||
-											`https://firebasestorage.googleapis.com/v0/b/youtube-clone-328013.appspot.com/o/assets%2Fimages%2Fprofile_placeholder.png?alt=media&token=d1bc2f1c-9c82-4f41-b255-1bf9413fa641`
-										}
-										alt=''
-									/>
-									<div className='metadata'>
-										<h3 className='video-title'>
-											{video.videoData.snippet.title}
-										</h3>
-										<div>
-											<h3 className='channel-name'>
-												{
-													video.videoData.snippet
-														.channelTitle
-												}
-											</h3>
-											<span>
-												<h4 className='total-views'>
-													{countFormatter(
-														stats.viewCount,
-														1
-													)}
-												</h4>
-												<h4 className='upload-date'>
-													{dateFormatter(
-														video.videoData.snippet
-															.publishedAt
-													)}
-												</h4>
-											</span>
-										</div>
-									</div>
-								</div>
-							</Link>
+							<ContentCardWrapper
+								video={video}
+								index={index}
+								loadVideo={loadVideo}
+								stats={stats}
+								channel={channel}
+							/>
 						);
 					}
 				);
@@ -146,60 +100,13 @@ const App = () => {
 				return Promise.all([videoStats, channelData]).then(
 					([stats, channel]) => {
 						return (
-							<Link
-								to={`watch=${video.videoData.id.videoId}`}
-								key={index}
-								className='content-card'
-								onClick={() =>
-									loadVideo({ video, stats, channel })
-								}
-							>
-								<img
-									className='card-thumbnail'
-									src={
-										video.videoData.snippet.thumbnails
-											.medium.url
-									}
-									alt='Video thumbnail'
-								/>
-								<div className='card-details'>
-									<img
-										className='channel-picture'
-										src={
-											channel.thumbnails.default.url ||
-											`https://firebasestorage.googleapis.com/v0/b/youtube-clone-328013.appspot.com/o/assets%2Fimages%2Fprofile_placeholder.png?alt=media&token=d1bc2f1c-9c82-4f41-b255-1bf9413fa641`
-										}
-										alt=''
-									/>
-									<div className='metadata'>
-										<h3 className='video-title'>
-											{video.videoData.snippet.title}
-										</h3>
-										<div>
-											<h3 className='channel-name'>
-												{
-													video.videoData.snippet
-														.channelTitle
-												}
-											</h3>
-											<span>
-												<h4 className='total-views'>
-													{countFormatter(
-														stats.viewCount,
-														1
-													)}
-												</h4>
-												<h4 className='upload-date'>
-													{dateFormatter(
-														video.videoData.snippet
-															.publishedAt
-													)}
-												</h4>
-											</span>
-										</div>
-									</div>
-								</div>
-							</Link>
+							<ContentCardWrapper
+								video={video}
+								index={index}
+								loadVideo={loadVideo}
+								stats={stats}
+								channel={channel}
+							/>
 						);
 					}
 				);
