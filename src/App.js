93ae006604b6494d60ff4700b3d10dd06c01fe54
mrjackmaster <jacektrg+github@gmail.com>
2021-10-19 18:45:16 +0200

    Moving functions creating displayed content to Content component, seems like a more suitable location for it. General cleanup.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import { app, myAPIKey } from './firebase';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { app } from './firebase';
+import { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
 import MenuVideo from './components/MenuVideo';
 import Content from './components/Content';
 import Video from './components/Video';
-import dateFormatter from './components/utils/dateFormatter';
-import countFormatter from './components/utils/countFormatter';
-import getRandomVideosFromFirestore from './components/utils/getRandomVideosFromFirestore';
-import getYTTrendingVideos from './components/utils/getYTTrendingVideos';
-import getYTVideoStatistics from './components/utils/getYTVideoStatistics';
-import getYTChannelData from './components/utils/getYTChannelData';
-import ContentCardWrapper from './components/utils/ContentCardWrapper';
 
 const App = () => {
 	const [menuIsThin, setMenuIsThin] = useState(false);
 	const [menuIsCollapsed, setMenuIsCollapsed] = useState(true);
 	const [searchInput, setSearchInput] = useState();
 	const [loadedVideoData, setLoadedVideoData] = useState();
-	const [mainContentDisplay, setMainContentDisplay] = useState();
-	const [trendingContentDisplay, setTrendingContentDisplay] = useState();
 
 	const toggleMenuWidth = () => {
 		setMenuIsThin((prevState) => !prevState);
@@ -45,76 +36,6 @@ const App = () => {
 		setLoadedVideoData(vidData);
 	};
 
-	useEffect(() => {
-		getRandomVideosFromFirestore(24)
-			.then((randomData) => {
-				createMainDisplayContent(randomData);
-			})
-			.then(async () => {
-				const trending = await getYTTrendingVideos(12, myAPIKey);
-				createTrendingDisplayContent(trending);
-			});
-	}, []);
-
-	const createMainDisplayContent = (data) => {
-		const mainPromise = Promise.all(
-			data.map(async (video, index) => {
-				const videoStats = await getYTVideoStatistics(
-					// Sometimes throws undefined error, need to find out why.
-					video.videoData.id.videoId,
-					myAPIKey
-				);
-				const channelData = await getYTChannelData(
-					video.videoData.snippet.channelId,
-					myAPIKey
-				);
-				return Promise.all([videoStats, channelData]).then(
-					([stats, channel]) => {
-						return (
-							<ContentCardWrapper
-								video={video}
-								index={index}
-								loadVideo={loadVideo}
-								stats={stats}
-								channel={channel}
-							/>
-						);
-					}
-				);
-			})
-		);
-		mainPromise.then((content) => setMainContentDisplay(content));
-	};
-
-	const createTrendingDisplayContent = (data) => {
-		const trendingPromise = Promise.all(
-			data.map(async (video, index) => {
-				const videoStats = await getYTVideoStatistics(
-					video.videoData.id.videoId,
-					myAPIKey
-				);
-				const channelData = await getYTChannelData(
-					video.videoData.snippet.channelId,
-					myAPIKey
-				);
-				return Promise.all([videoStats, channelData]).then(
-					([stats, channel]) => {
-						return (
-							<ContentCardWrapper
-								video={video}
-								index={index}
-								loadVideo={loadVideo}
-								stats={stats}
-								channel={channel}
-							/>
-						);
-					}
-				);
-			})
-		);
-		trendingPromise.then((content) => setTrendingContentDisplay(content));
-	};
-
 	return (
 		<BrowserRouter>
 			<Switch>
@@ -125,10 +46,7 @@ const App = () => {
 						handle={handleInput}
 					/>
 					<Menu isThin={menuIsThin} />
-					<Content
-						content={mainContentDisplay}
-						trending={trendingContentDisplay}
-					/>
+					<Content loadVideo={loadVideo} />
 				</Route>
 				<Route exact path='/watch=([\w-]{11,})'>
 					<Nav
