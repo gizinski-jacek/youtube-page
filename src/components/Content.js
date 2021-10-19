d6add2a048408a669330c1367950c20d8fec96c6
mrjackmaster <jacektrg+github@gmail.com>
2021-10-19 18:47:56 +0200

    Moving functionalities. Sligthly reworking content creating functions, also adding .catches. Similar to related videos, trending videos will get fetched after clicking button in order to save API tokens.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,101 @@
+import { myAPIKey } from '../firebase';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import getRandomVideosFromFirestore from './utils/getRandomVideosFromFirestore';
+import getYTTrendingVideos from './utils/getYTTrendingVideos';
+import getYTVideoStatistics from './utils/getYTVideoStatistics';
+import getYTChannelData from './utils/getYTChannelData';
+import ContentCardWrapper from './utils/ContentCardWrapper';
 
-const Content = ({ content, trending }) => {
+const Content = ({ loadVideo }) => {
+	const [showLoadBox, setShowLoadBox] = useState(true);
+	const [mainContentDisplay, setMainContentDisplay] = useState();
+	const [trendingContentDisplay, setTrendingContentDisplay] = useState();
+
+	const handleLoad = async () => {
+		setShowLoadBox(false);
+		const relatedData = await getYTTrendingVideos(12, myAPIKey);
+		createTrendingDisplayContent(relatedData);
+	};
+
+	useEffect(() => {
+		getRandomVideosFromFirestore(24).then((randomData) => {
+			createMainDisplayContent(randomData);
+		});
+	}, []);
+
+	const createMainDisplayContent = (data) => {
+		Promise.all(
+			data.map((video, index) => {
+				const videoStats = getYTVideoStatistics(
+					video.videoData.id.videoId,
+					myAPIKey
+				);
+				const channelData = getYTChannelData(
+					video.videoData.snippet.channelId,
+					myAPIKey
+				);
+				return Promise.all([videoStats, channelData])
+					.then(([stats, channel]) => {
+						return (
+							<ContentCardWrapper
+								key={index}
+								video={video}
+								loadVideo={loadVideo}
+								stats={stats}
+								channel={channel}
+							/>
+						);
+					})
+					.catch((error) => {
+						console.log(
+							`Promise all for main video statistics and channel data error: ${error}`
+						);
+					});
+			})
+		)
+			.then((content) => setMainContentDisplay(content))
+			.catch((error) => {
+				console.log(`Promise all for main content error: ${error}`);
+			});
+	};
+
+	const createTrendingDisplayContent = (data) => {
+		Promise.all(
+			data.map((video, index) => {
+				const videoStats = getYTVideoStatistics(
+					video.videoData.id.videoId,
+					myAPIKey
+				);
+				const channelData = getYTChannelData(
+					video.videoData.snippet.channelId,
+					myAPIKey
+				);
+				return Promise.all([videoStats, channelData])
+					.then(([stats, channel]) => {
+						return (
+							<ContentCardWrapper
+								key={index}
+								video={video}
+								loadVideo={loadVideo}
+								stats={stats}
+								channel={channel}
+							/>
+						);
+					})
+					.catch((error) => {
+						console.log(
+							`Promise all for trending video statistics and channel data error: ${error}`
+						);
+					});
+			})
+		)
+			.then((content) => setTrendingContentDisplay(content))
+			.catch((error) => {
+				console.log(`Promise all for trending content error: ${error}`);
+			});
+	};
+
 	return (
 		<div id='content-container'>
 			<div id='category-filter'>
@@ -27,10 +122,29 @@ const Content = ({ content, trending }) => {
 				</ul>
 			</div>
 			<div className='content-display'>
-				{content}
+				{mainContentDisplay}
 				<div className='trending-content-display'>
 					<h2 className='trending-tag'>Trending</h2>
-					{trending}
+					<div
+						className='relative-trending-container'
+						style={{ display: showLoadBox ? 'block' : 'none' }}
+					>
+						<div className='load-trending-videos'>
+							<h3>
+								Press the button to load trending videos. This
+								action uses a lot of API tokens and after few
+								uses will reach the daily quota which will
+								result in no videos being loaded anymore.
+							</h3>
+							<button
+								id='load-trending-videos-btn'
+								onClick={handleLoad}
+							>
+								Load Videos
+							</button>
+						</div>
+					</div>
+					{trendingContentDisplay}
 				</div>
 			</div>
 		</div>
