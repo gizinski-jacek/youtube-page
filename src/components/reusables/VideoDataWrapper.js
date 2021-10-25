3f2644bdda7916d63abfaf26cd5ada5a0e6a90f8
mrjackmaster <jacektrg+github@gmail.com>
2021-10-25 21:24:03 +0200

    Cleanup

--- src/components/reusables/VideoDataWrapper.js
+++ src/components/reusables/VideoDataWrapper.js
@@ -2,11 +2,7 @@ import { Link } from 'react-router-dom';
 import countFormatter from '../utils/countFormatter';
 import dateFormatter from '../utils/dateFormatter';
 
-const VideoDataWrapper = ({ video, loadVideo, stats, channel }) => {
-	const moreMenu = () => {
-		console.log('reminder');
-	};
-
+const VideoDataWrapper = ({ video, stats, channel, loadVideo }) => {
 	return (
 		<div className='video-card'>
 			<Link
@@ -54,7 +50,7 @@ const VideoDataWrapper = ({ video, loadVideo, stats, channel }) => {
 						</h4>
 					</span>
 				</div>
-				<div className='trending-card-more' onClick={moreMenu}>
+				<div className='trending-card-more'>
 					<svg focusable='false'>
 						<path d='M12,16.5c0.83,0,1.5,0.67,1.5,1.5s-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S11.17,16.5,12,16.5z M10.5,12 c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5s-0.67-1.5-1.5-1.5S10.5,11.17,10.5,12z M10.5,6c0,0.83,0.67,1.5,1.5,1.5 s1.5-0.67,1.5-1.5S12.83,4.5,12,4.5S10.5,5.17,10.5,6z'></path>
 					</svg>
