3e7c8b82253249272a1df2ff99dd34e4c066129a
mrjackmaster <jacektrg+github@gmail.com>
2021-10-26 14:20:49 +0200

    Moved out fetch functionality to MainPage.

--- src/components/reusables/GridContentsWrapper.js
+++ src/components/reusables/GridContentsWrapper.js
@@ -1,46 +1,22 @@
-import { myAPIKey } from '../../firebase';
-import getVideoStatistics from '../utils/getVideoStatistics';
-import getChannelData from '../utils/getChannelData';
 import VideoDataWrapper from './VideoDataWrapper';
 
-const GridContentsWrapper = async (data, loadVideo) => {
-	try {
-		return await Promise.all(
-			data.map(async (video, index) => {
-				const videoStats = getVideoStatistics(
-					video.videoData.id.videoId,
-					myAPIKey
-				);
-				const channelData = getChannelData(
-					video.videoData.snippet.channelId,
-					myAPIKey
-				);
-				try {
-					const [stats, channel] = await Promise.all([
-						videoStats,
-						channelData,
-					]);
-					if (stats && channel) {
-						return (
-							<VideoDataWrapper
-								key={index}
-								video={video}
-								stats={stats}
-								channel={channel}
-								loadVideo={loadVideo}
-							/>
-						);
-					}
-				} catch (error) {
-					console.log(
-						`Promise all for video statistics and channel data error: ${error}`
-					);
-				}
-			})
-		);
-	} catch (error) {
-		console.log(`Promise all for grid contents error: ${error}`);
-	}
+const GridContentsWrapper = ({ data, loadVideo }) => {
+	const contents = data.map((item, index) => {
+		if (item.video && item.statsData && item.channelData) {
+			return (
+				<VideoDataWrapper
+					key={index}
+					video={item.video}
+					stats={item.statsData}
+					channel={item.channelData}
+					loadVideo={loadVideo}
+				/>
+			);
+		}
+		return null;
+	});
+
+	return contents;
 };
 
 export default GridContentsWrapper;
