8e97c423cc0bada215fd87320b4d99d5c307e070
mrjackmaster <jacektrg+github@gmail.com>
2021-10-26 14:38:01 +0200

    Moved out fetch functionality to VideoPage.

--- src/components/reusables/RelatedContentsWrapper.js
+++ src/components/reusables/RelatedContentsWrapper.js
@@ -1,46 +1,22 @@
-import { myAPIKey } from '../../firebase';
-import getVideoStatistics from '../utils/getVideoStatistics';
-import getChannelData from '../utils/getChannelData';
 import RelatedDataWrapper from './RelatedDataWrapper';
 
-const RelatedContentsWrapper = async (data, loadVideo) => {
-	try {
-		return await Promise.all(
-			data.map(async (video, index) => {
-				const videoStats = getVideoStatistics(
-					video.videoData.id.videoId,
-					myAPIKey
-				);
-				const channelData = getChannelData(
-					video.videoData.snippet.channelId,
-					myAPIKey
-				);
-				try {
-					const [stats, channel] = await Promise.all([
-						videoStats,
-						channelData,
-					]);
-					if (stats && channel) {
-						return (
-							<RelatedDataWrapper
-								key={index}
-								video={video}
-								stats={stats}
-								channel={channel}
-								loadVideo={loadVideo}
-							/>
-						);
-					}
-				} catch (error) {
-					console.log(
-						`Promise all for related video statistics and channel data error: ${error}`
-					);
-				}
-			})
-		);
-	} catch (error) {
-		console.log(`Promise all for related content error: ${error}`);
-	}
+const RelatedContentsWrapper = ({ data, loadVideo }) => {
+	const contents = data.map((item, index) => {
+		if (item.video && item.statsData && item.channelData) {
+			return (
+				<RelatedDataWrapper
+					key={index}
+					video={item.video}
+					stats={item.statsData}
+					channel={item.channelData}
+					loadVideo={loadVideo}
+				/>
+			);
+		}
+		return null;
+	});
+
+	return contents;
 };
 
 export default RelatedContentsWrapper;
