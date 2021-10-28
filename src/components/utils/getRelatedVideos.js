3817c2c7618ac778344d61a901888d58c403fd14
mrjackmaster <jacektrg+github@gmail.com>
2021-10-28 17:03:23 +0200

    Cleanup, order by relevance is the default value.

--- src/components/utils/getRelatedVideos.js
+++ src/components/utils/getRelatedVideos.js
@@ -1,7 +1,7 @@
 const getRelatedVideos = async (videoId, APIKey) => {
 	try {
 		const response = await fetch(
-			`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&order=relevance&relatedToVideoId=${videoId}&type=video&key=${APIKey}`
+			`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&relatedToVideoId=${videoId}&type=video&key=${APIKey}`
 		);
 		const data = await response.json();
 		const array = data.items.map((item) => {
